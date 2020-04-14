ce005a81ba135247d06e0e81c1b6c65a29f8f0e3
Connie Tran <connietrandev@gmail.com>
2020-04-13 22:06:42 -0400

    Base HTML CSS and JS

---
+++ app.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Card options
+    const cardArray = [
+        {
+            name: 'cake',
+            img: 'gifs/cake.gif'
+        },
+        {
+            name: 'cake',
+            img: 'gifs/cake.gif'
+        },
+        {
+            name: 'cookie',
+            img: 'gifs/cookie.gif'
+        },
+        {
+            name: 'cookie',
+            img: 'gifs/cookie.gif'
+        },
+        {
+            name: 'cotton-candy',
+            img: 'gifs/cotton-candy.gif'
+        },
+        {
+            name: 'cotton-candy',
+            img: 'gifs/cotton-candy.gif'
+        },
+        {
+            name: 'doughnut',
+            img: 'gifs/doughnut.gif'
+        },
+        {
+            name: 'doughnut',
+            img: 'gifs/doughnut.gif'
+        },
+        {
+            name: 'ice-cream',
+            img: 'gifs/ice-cream.gif'
+        },
+        {
+            name: 'ice-cream',
+            img: 'gifs/ice-cream.gif'
+        },
+        {
+            name: 'pineapple',
+            img: 'gifs/pineapple.gif'
+        },
+        {
+            name: 'pineapple',
+            img: 'gifs/pineapple.gif'
+        },
+        {
+            name: 'watermelon',
+            img: 'gifs/watermelon.gif'
+        },
+        {
+            name: 'watermelon',
+            img: 'gifs/watermelon.gif'
+        }
+    ]
+});
