24878643a516921c8530708dbf1093876ac98f73
Connie Tran <connietrandev@gmail.com>
2020-04-19 18:23:45 -0400

    Add flipCard() function

--- app.js
+++ app.js
@@ -53,19 +53,47 @@ document.addEventListener('DOMContentLoaded', () => {
     ]
 
     const grid = document.querySelector('.grid');
+    var cardsChosen = [];
+    var cardsChosenId = [];
 
     // Create the board
     function createBoard() {
+        // For cardArray
         for (let i = 0; i < cardArray.length; i++) {
+            
+            // Set images with data-id = index
             var card = document.createElement('img');
             card.setAttribute('src', 'images/blank.png');
             card.setAttribute('data-id', i);
             card.setAttribute('class', 'card');
-            // card.addEventListener('click', flipCard);
+
+            card.addEventListener('click', flipCard);
+
             grid.appendChild(card);
         };
 
     };
+
+    // Flip card function
+    function flipCard() {
+        
+        // When flipped, get data-id of img
+        var cardId = this.getAttribute('data-id');
+        
+        // Push chosen card onto arrays
+        cardsChosen.push(cardArray[cardId].name);
+        cardsChosenId.push(cardId);
+        
+        // Set image based on data-id
+        this.setAttribute('src', cardArray[cardId].img);
+
+        // If user has chosen 2 cards, run checkforMatch function
+        // setTimeout to 500 ms give some buffer time
+        if (cardsChosen.length === 2) {
+            setTimeout(checkforMatch, 500);
+        }
+
+    }
 
     createBoard();
 
